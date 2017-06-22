8e143a6e7fa99e26789ebac423b1bfd273f71190
takahashim <maki@rubycolor.org>
2017-06-22 20:29:48 +0900

    rename package

--- aozora_conv_test.go
+++ aozora_conv_test.go
@@ -1,4 +1,4 @@
-package main
+package aozoraconv
 
 import (
 	"bytes"
--- cmd/aozoraconv/main.go
+++ cmd/aozoraconv/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/takahashim/aozoraconv"
 )
 
 func main() {
 	var str = "—―＼〜～‖∥−－¥￥¢￠£￡¬￢｜・◯"
-	fmt.Println(AozoraConv(str))
+	fmt.Println(aozoraconv.AozoraConv(str))
 	fmt.Printf("aaa")
 }
