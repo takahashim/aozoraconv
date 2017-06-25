56baaf7f61edf0a98c05416d94f6f9776f41d7dd
takahashim <maki@rubycolor.org>
2017-06-25 22:01:18 +0900

    fix error handing, add -e option

--- cmd/aozoraconv/main.go
+++ cmd/aozoraconv/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-
 	"io"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/takahashim/aozoraconv"
 	"golang.org/x/text/encoding/japanese"
@@ -15,18 +14,29 @@ import (
 )
 
 func main() {
+	os.Exit(doMain())
+}
+
+func errorf(format string, a ...interface{}) (ret int, err error) {
+	ret, err = fmt.Fprintf(os.Stderr, format+"\n", a...)
+	return ret, err
+}
+
+func doMain() int {
 	const encSjis = 1
 	const encUtf8 = 2
 
 	var useSjis, useUtf8 bool
 	var enc int
 	var path, outpath string
+	var encoding string
 	var input io.Reader
 	var output io.Writer
 	var err error
 
-	flag.BoolVar(&useSjis, "sjis", false, "convert from UTF-8 into Shift_JIS")
-	flag.BoolVar(&useUtf8, "utf8", false, "convert from Shift_JIS into UTF-8")
+	flag.StringVar(&encoding, "e", "sjis", "set output encoding (sjis or utf8)")
+	flag.BoolVar(&useSjis, "s", false, "convert from UTF-8 into Shift_JIS")
+	flag.BoolVar(&useUtf8, "u", false, "convert from Shift_JIS into UTF-8")
 	flag.StringVar(&path, "f", "", "input filename")
 	flag.StringVar(&outpath, "o", "", "output filename")
 
@@ -37,7 +47,8 @@ func main() {
 	} else {
 		input, err = os.Open(path)
 		if err != nil {
-			log.Fatalf("error: %v", err)
+			errorf("error: %v", err)
+			return 1
 		}
 	}
 
@@ -46,14 +57,18 @@ func main() {
 	} else {
 		output, err = os.OpenFile(outpath, os.O_WRONLY|os.O_CREATE, 0600)
 		if err != nil {
-			log.Fatal(err)
+			errorf("%s", err)
+			return 1
 		}
 	}
 
-	if useUtf8 {
+	if strings.ToLower(encoding) == "utf8" || strings.ToLower(encoding) == "utf-8" || useUtf8 {
 		enc = 2
-	} else {
+	} else if strings.ToLower(encoding) == "sjis" || strings.ToLower(encoding) == "shift_jis" || useSjis {
 		enc = 1
+	} else {
+		errorf("define encoding -s (Shift_JIS) or -u (UTF-8) or -e sting")
+		return 1
 	}
 
 	if enc == encUtf8 {
@@ -61,24 +76,29 @@ func main() {
 		reader := transform.NewReader(input, decoder)
 		ret, err := ioutil.ReadAll(reader)
 		if err != nil {
-			log.Fatal(err)
+			errorf("error: %v", err)
+			return 1
 		}
 		str := aozoraconv.ConvRev(string(ret))
 		_, err = fmt.Fprint(output, str)
 		if err != nil {
-			log.Fatal(err)
+			errorf("error: %v", err)
+			return 1
 		}
 	} else {
 		ret, err := ioutil.ReadAll(input)
 		if err != nil {
-			log.Fatal(err)
+			errorf("error: %v", err)
+			return 1
 		}
 		str := aozoraconv.Conv(string(ret))
 		encoder := japanese.ShiftJIS.NewEncoder()
 		writer := transform.NewWriter(output, encoder)
 		_, err = fmt.Fprint(writer, str)
 		if err != nil {
-			log.Fatal(err)
+			errorf("error: %v", err)
+			return 1
 		}
 	}
+	return 0
 }
