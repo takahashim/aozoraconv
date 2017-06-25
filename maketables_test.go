c0fbe2c1c45813ed369e6d8f5bf0e7088dc4528a
takahashim <maki@rubycolor.org>
2017-06-25 14:32:02 +0900

    fix tests

--- maketables_test.go
+++ maketables_test.go
@@ -9,14 +9,14 @@ func TestParseLine(test *testing.T) {
 	var s string
 	m, k, t, uni, uni2 := 0, 0, 0, int32(0), int32(0)
 	s = "3-2121\tU+3000\t# IDEOGRAPHIC SPACE"
-	err = parseLine(s, &m, &k, &t, &uni, &uni2)
+	err = ParseLine(s, &m, &k, &t, &uni, &uni2)
 	if m != 3 || k != 0x21 || t != 0x21 || uni != 0x3000 || uni2 != 0 {
 		test.Errorf("AozoraConv: m,k,t,uni,uni2: %v,%X,%X,%X,%X\nerr:%q\ns:%q",
 			m, k, t, uni, uni2, err, s)
 	}
 
 	s = "3-2477	U+304B+309A	# 	[2000]"
-	err = parseLine(s, &m, &k, &t, &uni, &uni2)
+	err = ParseLine(s, &m, &k, &t, &uni, &uni2)
 	if m != 3 || k != 0x24 || t != 0x77 || uni != 0x304B || uni2 != 0x309A {
 		test.Errorf("AozoraConv: m,k,t,uni,uni2: %v,%X,%X,%X,%X\nerr:%q\ns:%q",
 			m, k, t, uni, uni2, err, s)
